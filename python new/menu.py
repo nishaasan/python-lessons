6f84ed56951c223939bfead366ae36aa4d4b714d
nishaasan <86878032+nishaasan@users.noreply.github.com>
2023-11-02 08:25:12 +0530

    Add files via upload

---
+++ python new/menu.py
@@ -0,0 +1,6 @@
+# Build a menu
+title = "menu".upper()
+print(title.center(20, "="))
+print("Coffee".ljust(16, ".") + "$1".rjust(4))
+print("Muffin".ljust(16, ".") + "$2".rjust(4))
+print("Cheesecake".ljust(16, ".") + "$4".rjust(4))
