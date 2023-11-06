7b8e3d4f51d3c9f7677ec115ff11d617326b800d
nishaasan <86878032+nishaasan@users.noreply.github.com>
2023-11-06 15:16:06 +0530

    Add files via upload

---
+++ python new/if/condition.py
@@ -0,0 +1,36 @@
+# # #fstring
+# # name="python"
+# # print("hi",name)
+# # #fsting
+# # print(f"I love {name}")
+
+
+# num1=15
+# if num1>5:
+#   print(f"{num1} is greater than 5")
+#   if num1>10:
+#     print(f"{num1} is greayter than 10")
+
+# english=int(input("enter english marks: "))
+# cs=int(input("enter cs marks: "))
+# maths=int(input("enter maths marks: "))
+# CGPA=(english+cs+maths)/3
+# print(CGPA)
+# if CGPA>80:
+#   print(f"is ur {CGPA},so u scored A grade")
+#   if CGPA>90:
+#     print("Excellent")
+# elif CGPA>60 and CGPA<80:
+#   print(f"is ur {CGPA},so u scored B grade")
+# elif CGPA>40 and CGPA<60:
+#   print(f"is ur {CGPA},so u scored C grade")
+# else:
+#   print(f"is ur {CGPA},so u scored D grade")
+
+
+lis1=[1,2,3]
+tup1=(1,2,3)
+# if 2 in lis1:
+#   print("s")
+if 2 in tup1:
+  print("s")
