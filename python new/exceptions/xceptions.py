488c5fdb84b2a391071232dfeeafa369b98dbe68
nishaasan <86878032+nishaasan@users.noreply.github.com>
2023-11-25 11:05:49 +0530

    Add files via upload

---
+++ python new/exceptions/xceptions.py
@@ -0,0 +1,20 @@
+a = 5
+b = 2
+
+try:
+    print("resource Open")
+    print(a/b)
+    k = int(input("Enter a number"))
+    print(k)
+
+except ZeroDivisionError as e:
+    print("Hey, You cannot divide a Number by Zero" , e)
+
+except ValueError as e:
+    print("Invalid Input",e)
+
+except Exception as e:
+    print("Something went Wrong...")
+
+finally:
+    print("resource Closed")
